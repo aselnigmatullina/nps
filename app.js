dc9aff9cd86b74f1b3ad7f656f8a64e654319a8c
Student <student@itpark.ru>
2019-09-21 09:34:54 +0300

    работающий расчет для трех человек

--- app.js
+++ app.js
@@ -1,1 +1,32 @@
-console.log('worked');
+const firstVote=10; //оценка первого
+const secondVote=10; //оценка второго
+const thirdVote=10;
+
+let promotersCount = 0;
+let detractorsCount=0;
+
+if(firstVote >= 9){
+  promotersCount = promotersCount+1;
+}
+if(firstVote<=6){
+    detractorsCount= detractorsCount+1;
+}
+if(secondVote >= 9){
+    promotersCount = promotersCount+1;
+  }
+if(secondVote<=6){
+      detractorsCount= detractorsCount+1;
+  }
+if(thirdVote >= 9){
+    promotersCount = promotersCount+1;
+  }
+if(thirdVote<=6){
+      detractorsCount= detractorsCount+1;
+  }
+const nps = (promotersCount/3-detractorsCount/3)*100;
+console.log(nps);
+console.log(promotersCount);
+console.log(detractorsCount);
+
+
+
